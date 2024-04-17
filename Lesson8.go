c6994d644f3014f7af47fa9198a9a4aca23bff94
Anna <thginerucsbo@gmail.com>
2024-04-17 15:33:55 +0300

    Решения задач к 8 лекции

---
+++ Lesson8.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	map1 := map[string]string{
+		"key_1": "слон",
+		"key_2": "бегемот",
+		"key_3": "носорог",
+		"key_4": "лев",
+	}
+	fmt.Println("карта map1: ", map1)
+}
