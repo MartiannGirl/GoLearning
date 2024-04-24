9415873783996f2210943292c227091f2938ae76
Anna <thginerucsbo@gmail.com>
2024-04-24 21:15:25 +0300

    Решения задач к 10 лекции

--- Lesson10.go
+++ Lesson10.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	msg := fmt.Sprintf("https://vk.com/video-84632605_456242835 ", first.Hello())
+	msg := fmt.Sprintf("Hello, Dolly! ", "https://vk.com/video-84632605_456242835 ", first.Hello())
 
 	fmt.Println(msg)
 }
